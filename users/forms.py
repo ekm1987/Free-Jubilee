0ad94f72b810bf564764e7bb770cc0fcb94f8b8c
Emma Mondy <ekm1987@hotmail.com>
2019-10-05 08:47:12 +0800

    Resolve password save issue

--- users/forms.py
+++ users/forms.py
@@ -7,33 +7,13 @@ from django.forms import ModelForm, PasswordInput
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Div, Submit, Row, Column, Field
 
-class CustomUserCreationForm(forms.ModelForm):
+class CustomUserCreationForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
     email = forms.EmailField(max_length=254, required=True, help_text='Valid email address required.')
-    re_password = forms.CharField(max_length=128, widget=forms.PasswordInput, validators=[validate_password], label='Re-enter password')
-
-    def __init__(self, *args, **kwargs):
-        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
-        for re_password, field in self.fields.items():
-            field.required = True
-        password_field = self.fields['password']
-        password_field.validators.append(MinLengthValidator(limit_value=8))
-
-    def clean(self):
-        super(CustomUserCreationForm, self).clean()
-        password = self.cleaned_data.get('password')
-        re_password = self.cleaned_data.get('re_password')
-        if not password == re_password:
-            self.add_error('re_password', 'Passwords must match')
-        password.save()
-
     class Meta:
         model = CustomUser
-        fields = ('username', 'first_name', 'last_name', 'email', 'password', 're_password',)
-        widgets = {
-            'password': PasswordInput()
-        }
+        fields = ('username', 'first_name', 'last_name', 'email',)
 
 class CustomUserChangeForm(UserChangeForm):
     first_name = forms.CharField(max_length=30, required=True)
--- users/views.py
+++ users/views.py
@@ -1,10 +1,15 @@
 from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth import login, authenticate
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import CustomUser
 from .forms import CustomUserCreationForm, UserChangeForm
 
 
+@login_required
+def home(request):
+    return render(request, 'freeJubileeApp/index.html')
 
 class AccountView(generic.DetailView):
   model = CustomUser
@@ -16,7 +21,19 @@ class Register(generic.CreateView):
   success_url = reverse_lazy("freeJubileeApp:index")
   template_name = "registration/register.html"
 
+  def register(self, request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
 class ChangeUser(generic.UpdateView):
   form_class = UserChangeForm
   success_url = reverse_lazy('changeUser')
   template_name = 'changeUser.html'
+
